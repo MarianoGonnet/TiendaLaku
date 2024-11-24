c45b9b245d418e18a2da16b65def06371194696f
Mariano <gonnet771@gmail.com>
2024-11-24 13:51:37 -0300

    Se linkeo ws, mail e instagram funcionando directo

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,9 +4,9 @@ import '../Styles/SocialLinks.css'; // Ruta corregida
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-      <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">TikTok</a>
+      <a href="https://wa.me/5492235347158" target="_blank" rel="noopener noreferrer">Whatsapp</a>
+      <a href="https://www.instagram.com/laku.indumentaria/" target="_blank" rel="noopener noreferrer">Instagram</a>
+      <a href="mailto:nahuel_19_08@hotmail.com" target="_blank" rel="noopener noreferrer">Email</a>
     </div>
   );
 };
