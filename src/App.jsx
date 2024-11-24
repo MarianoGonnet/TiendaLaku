5d636f7e3e2c86cd7f57f8e6a4c035d7afd092e6
Mariano <gonnet771@gmail.com>
2024-11-24 17:00:34 -0300

    Se mejoro sobre contacto.html un poco

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import SocialLinks from './components/SocialLinks';
+import './styles/contacto.css';
 
 const App = () => {
   return (
