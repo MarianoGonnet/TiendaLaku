2ee57d37a8f0b834cbe95bb1f0602c09f796496c
Alexis Kees <alexiskeesbahl@gmail.com>
2024-11-28 12:08:33 -0300

    agregué barra de flotantes como componente y puse los iconos en blanco (en hero page)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import SocialLinks from './components/SocialLinks';
+import Flotantes from './components/flotantes';
 import './styles/contacto.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <SocialLinks />
+      <Flotantes/>
     </div>
   );
 };
