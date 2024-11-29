58924f1786c9935524cf6d2f3303769913e90de8
Alexis Kees <alexiskeesbahl@gmail.com>
2024-11-29 01:18:43 -0300

    modifiqué footer para que quede como en mockups (disposición, contenido y fuente)

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,9 +4,10 @@ import '../Styles/SocialLinks.css'; // Ruta corregida
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <a href="https://wa.me/5492235347158" target="_blank" rel="noopener noreferrer">Whatsapp</a>
-      <a href="https://www.instagram.com/laku.indumentaria/" target="_blank" rel="noopener noreferrer">Instagram</a>
-      <a href="../src/styles/contacto.html" target="_blank" rel="noopener noreferrer">Contacto</a>
+      <a href='../../index.html' rel="noopener noreferrer">Inicio</a>
+      <a href="../src/styles/galeria.html" rel="noopener noreferrer">Galeria</a>
+      <a href="../src/styles/contacto.html" rel="noopener noreferrer">Contacto</a>
+      <p>Copyright Tienda Laku Indumentaria Masculina - 2024. Todos los derechos reservados.</p>
     </div>
   );
 };
