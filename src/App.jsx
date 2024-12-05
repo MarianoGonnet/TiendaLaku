8c0f9ea5844d397e1e3819761c3512411e581df9
Alexis Kees <alexiskeesbahl@gmail.com>
2024-12-04 22:14:06 -0300

    cree archivos mobile y hook, aplique cambios en app.jsx para renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 import Hero from './components/Hero'
+import HeroMobile from './components/HeroMobile'
 import Contacto from './components/contacto'
+import ContactoMobile from './components/contactoMobile'
 import Galeria from './components/galeria'
+import GaleriaMobile from './components/galeriaMobile'
 import Flotantes from './components/flotantes'
 import Navbar from './components/Navbar'
+import NavbarMobile from './components/NavbarMobile'
 import SocialLinks from './components/SocialLinks'
 import './styles/fuentes.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import useTamañoVentana from './hooks/useTamañoVentana'
 
 function App() {
   const [count, setCount] = useState(0)
+  const esMobile = useTamañoVentana();
 
   return (
+
     <Router>
-            <Navbar />
-      {/* Rutas de la aplicación */}
-      <Routes>
-        <Route path='/' element={<Hero />} />    
-        <Route path='/contacto' element={<Contacto />} />
-        <Route path='/galeria' element={<Galeria />} />
-      </Routes>
+    {esMobile ? (
+      <NavbarMobile />
+    ) : (
+      <Navbar />
+    )}
+
+<Routes>
+  <Route path='/' element={ esMobile ? <HeroMobile/> : <Hero /> } />    
+  <Route path='/contacto' element={esMobile ? <ContactoMobile/> : <Contacto />} />
+  <Route path='/galeria' element={esMobile ? <GaleriaMobile/> : <Galeria />} />
+</Routes>
 
 
-      <SocialLinks />
-      <Flotantes/>
-    </Router>
+<SocialLinks />
+<Flotantes/>
+
+  </Router>
   )
 }
 
